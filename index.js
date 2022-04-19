347c42e707e799f7e1f5f5d4d2a28fa37425b124
Yuriy <xstream_06@mail.ru>
2022-04-20 01:52:47 +0300

    Lesson 1 HW

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 const colors = require("colors/safe");
 
 const isPrime = (number) => {
-    if (number < 2) return false;
+    if (number < 2)
+        return false;
 
     for (let i = 2; i <= number / 2; i++) {
-        if (number % i === 0) return false;
+        if (number % i === 0)
+            return false;
+
     }
 
     return true;
 };
 
 let count = 1;
+let mark = 0;
 
 const from = process.argv[2];
 const to = process.argv[3];
@@ -18,18 +22,21 @@ const to = process.argv[3];
 
 for (let number = from; number <= to; number++) {
     let colorer = colors.green;
-
     if (isPrime(number)) {
         if (count % 2 === 0) {
             colorer = colors.yellow;
             count += 1;
+            mark += 1;
         } else if (count % 3 === 0) {
             colorer = colors.red;
             count = 1;
+            mark += 1;
         } else {
             count += 1;
+            mark += 1;
         }
-
         console.log(colorer(number));
     }
 }
+if (mark == 0)
+    console.log(colors.red("numbers not found"));
