9b14fed214eb1bb5c3df39fe1ffdc5c9104d9b05
Yuriy <xstream_06@mail.ru>
2022-05-20 15:32:11 +0300

    HW update

--- search.js
+++ search.js
@@ -3,16 +3,26 @@ const readStream = new fs.ReadStream('./access.log', 'utf8');
 const writeStream = fs.createWriteStream('./processed.log', {
     flags: "a",
     encoding: "utf-8",
-  });
-const {Transform} = require('stream');
-const transformStream = new Transform ({
-    transform(chunk, encoding, callback){        
-        const transformedChunk = chunk.toString().match(/^89.123.1.41 .*/gm).join('\n');
-               
-        callback(transformedChunk);
-        
+});
+const { Transform } = require('stream');
+const transformStreamFirst = new Transform({
+    transform(chunk, encoding, callback) {
+        const transformedChunk1 = chunk.toString().match(/^89.123.1.41 .*/gm).join('\n');
+
+        callback(null, transformedChunk1);
+
+    }
+});
+const transformStreamSecond = new Transform({
+    transform(chunk, encoding, callback) {
+        const transformedChunk2 = chunk.toString().match(/^34.48.240.111 .*/gm).join('\n');
+
+        callback(null, transformedChunk2);
+
     }
 });
 
-readStream.pipe(transformStream).pipe(writeStream)
+readStream.pipe(transformStreamFirst).pipe(writeStream);
+readStream.pipe(transformStreamSecond).pipe(writeStream);
+
 
