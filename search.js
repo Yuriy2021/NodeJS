76de2416356113a46c774e1deabca8b439c0594a
Yuriy <xstream_06@mail.ru>
2022-05-20 20:29:48 +0300

    HW update

--- search.js
+++ search.js
@@ -1,28 +1,34 @@
 const fs = require('fs');
 const readStream = new fs.ReadStream('./access.log', 'utf8');
-const writeStream = fs.createWriteStream('./processed.log', {
+const writeStreamFirst = fs.createWriteStream('./%89.123.1.41%.log', {
+    flags: "a",
+    encoding: "utf-8",
+});
+
+
+const writeStreamSecond = fs.createWriteStream('./%34.48.240.111%.log', {
     flags: "a",
     encoding: "utf-8",
 });
 const { Transform } = require('stream');
 const transformStreamFirst = new Transform({
     transform(chunk, encoding, callback) {
-        const transformedChunk1 = chunk.toString().match(/^89.123.1.41 .*/gm).join('\n');
 
-        callback(null, transformedChunk1);
+        const transformedChunk = chunk.toString().match(/^89.123.1.41 .*/gm).join('\n');
+
+        callback(null, transformedChunk);
 
     }
 });
 const transformStreamSecond = new Transform({
     transform(chunk, encoding, callback) {
-        const transformedChunk2 = chunk.toString().match(/^34.48.240.111 .*/gm).join('\n');
 
-        callback(null, transformedChunk2);
+        const transformedChunk = chunk.toString().match(/^34.48.240.111 .*/gm).join('\n');
+
+        callback(null, transformedChunk);
 
     }
 });
 
-readStream.pipe(transformStreamFirst).pipe(writeStream);
-readStream.pipe(transformStreamSecond).pipe(writeStream);
-
-
+readStream.pipe(transformStreamFirst).pipe(writeStreamFirst);
+readStream.pipe(transformStreamSecond).pipe(writeStreamSecond);
