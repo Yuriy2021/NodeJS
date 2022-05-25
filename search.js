a24f8a6e2dd535850fffe4e1b697eb6499bae367
Yuriy <xstream_06@mail.ru>
2022-05-25 18:52:58 +0300

    Lesson4HW_update

--- search.js
+++ search.js
@@ -10,46 +10,46 @@ rl.question("Please enter the path to the file: ", function (inputedPath) {
     const filePath = path.join(__dirname, inputedPath);
     const readStream = new fs.ReadStream(filePath, 'utf8');
 
+
+
+
+    const writeStreamFirst = fs.createWriteStream('./%89.123.1.41%.log', {
+        flags: "a",
+        encoding: "utf-8",
+    });
+
+
+
+    const writeStreamSecond = fs.createWriteStream('./%34.48.240.111%.log', {
+        flags: "a",
+        encoding: "utf-8",
+    });
+    const { Transform } = require('stream');
+    const transformStreamFirst = new Transform({
+        transform(chunk, encoding, callback) {
+
+            const transformedChunk = chunk.toString().match(/^89.123.1.41 .*/gm).join('\n');
+            console.log(transformedChunk);
+
+            callback(null, transformedChunk);
+
+        }
+    });
+    const transformStreamSecond = new Transform({
+        transform(chunk, encoding, callback) {
+
+            const transformedChunk = chunk.toString().match(/^34.48.240.111 .*/gm).join('\n');
+
+            callback(null, transformedChunk);
+
+        }
+    });
+
+
     readStream.pipe(transformStreamFirst).pipe(writeStreamFirst);
     readStream.pipe(transformStreamSecond).pipe(writeStreamSecond);
     rl.close();
 });
-
-
-const writeStreamFirst = fs.createWriteStream('./%89.123.1.41%.log', {
-    flags: "a",
-    encoding: "utf-8",
-});
-
-
-
-const writeStreamSecond = fs.createWriteStream('./%34.48.240.111%.log', {
-    flags: "a",
-    encoding: "utf-8",
-});
-const { Transform } = require('stream');
-const transformStreamFirst = new Transform({
-    transform(chunk, encoding, callback) {
-
-        const transformedChunk = chunk.toString().match(/^89.123.1.41 .*/gm).join('\n');
-        console.log(transformedChunk);
-
-        callback(null, transformedChunk);
-
-    }
-});
-const transformStreamSecond = new Transform({
-    transform(chunk, encoding, callback) {
-
-        const transformedChunk = chunk.toString().match(/^34.48.240.111 .*/gm).join('\n');
-
-        callback(null, transformedChunk);
-
-    }
-});
-
-
-
 
 
 rl.on("close", function () {
