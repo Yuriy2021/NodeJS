07b60507d756d81d38fec0812d93cc91208e0fdb
Yuriy <xstream_06@mail.ru>
2022-05-26 13:23:03 +0300

    Lesson4HW_update

--- search.js
+++ search.js
@@ -12,43 +12,32 @@ inquirer
             message: "Choose directory",
 
         },
-
-
+        {
+            name: "fir_search",
+            type: "input",
+            message:"Enter the regular expression for search first IP:",
+        },
+        
     ])
 
     .then((answer) => {
         const filePath = answer.directory;
         const readStream = new fs.ReadStream(filePath, 'utf8');
-        const writeStreamFirst = fs.createWriteStream('./%89.123.1.41%.log', {
+        const writeStreamFirst = fs.createWriteStream(`./%${answer.fir_search}.log`, {
             flags: "a",
             encoding: "utf-8",
         });
 
-
-
-        const writeStreamSecond = fs.createWriteStream('./%34.48.240.111%.log', {
-            flags: "a",
-            encoding: "utf-8",
-        });
         const { Transform } = require('stream');
         const transformStreamFirst = new Transform({
             transform(chunk, encoding, callback) {
-
-                const transformedChunk = chunk.toString().match(/^89.123.1.41 .*/gm).join('\n');
-
-                callback(null, transformedChunk);
-
-            }
-        });
-        const transformStreamSecond = new Transform({
-            transform(chunk, encoding, callback) {
-
-                const transformedChunk = chunk.toString().match(/^34.48.240.111 .*/gm).join('\n');
+                const re = new RegExp(`^${answer.fir_search} .*`, 'gm')
+                const transformedChunk = chunk.toString().match(re).join('\n');
 
                 callback(null, transformedChunk);
 
             }
         });
-        readStream.pipe(transformStreamFirst).pipe(writeStreamFirst);
-        readStream.pipe(transformStreamSecond).pipe(writeStreamSecond);
+        
+        readStream.pipe(transformStreamFirst).pipe(writeStreamFirst);        
     });
